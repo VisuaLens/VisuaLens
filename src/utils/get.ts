7d7a76f7eab65c72caed59cc76424f49eb75ceab
the0hdDev <theowimber@gmail.com>
2024-09-06 19:57:48 +0200

    uuid added

--- src/utils/get.ts
+++ src/utils/get.ts
@@ -1,5 +1,6 @@
 // @ts-ignore
 import app from '../index'
+const uuid: any = require('uuid')
 let Vcounter: number = 0
 
 
@@ -12,11 +13,12 @@ export function getDate(): any {
 }
 
 export function getSId() {
-
+    const
 }
 
 export function getUId() {
-
+    const UId: number = uuid.v4();
+    return UId;
 }
 
 export function getVId() {
