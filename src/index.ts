e00a38c3ad61c6c7c14898a1ad615a14f9e88e69
the0hdDev <theowimber@gmail.com>
2024-09-07 18:04:09 +0200

    worked on webserveer

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ app.post('/api/Wvisit', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 // Server Start
