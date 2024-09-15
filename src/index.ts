f48ecf7ae48901fb9a7c8abaff69c3a1d5f77830
the0hdDev <theowimber@gmail.com>
2024-09-15 10:21:38 +0200

    intergration with mongodb

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import cors from 'cors';
 import express from 'express';
 import path from 'path';
 import { collectNwriteData } from './ts/collectData';
-import {getEvents} from "./utils/getEvents";
+// import {getEvents} from "./utils/getEvents";
 import {getUId} from "./utils/get";
 import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
 const moment = require('moment-timezone');
 const app = express();
 dotenv.config()
-const uri = process.env.MONGODB
+const uri = "mongodb+srv://theowimber:zSkOtFpI8Wk0X7x7@testmongodb.hvh0x.mongodb.net/?retryWrites=true&w=majority&appName=testmongodb"
 // const client = new MongoClient(uri)
 export {app};
 
@@ -52,7 +52,7 @@ app.listen(8000, () => {
 
 
 app.post('/api/event/button', (req: any, res: any) => {
-    console.log(getEvents(req));
+    console.log();
     res.status(200).send()
 })
 let uid: number = getUId();
@@ -69,22 +69,32 @@ app.get('/api/local/getuid', (req: any, res: any) => {
     res.status(200).send()
 })
 
-app.post('/api/local/senduid', (req: any, res: any) => {
-    uid = req.body()
-    
+async function writeUIDtoDB() {
+    // connect to mongodb 
+    const client = new MongoClient(uri)
+    const struid = uid.toString()
+    await client.connect
+    const db = client.db("main")
+    const visiscollection = db.collection("visits")
+    const testdata = {
+        "fortnite": [
+            "testdata", {
+              "testdata": "",
+              "testdata2": "duhuansognbliblablob",
+              "uid": uid,
+        },
+        "events", {
+            "testevent": "bli bla blub",
+            "testbuttonpress": "button bla pressed"
+        }
+    ]}
+    await visiscollection.insertOne(testdata)
+        // const result = await usersCollection.insertOne({ name: "Max", age: 30 });
+}
+
+app.post('/api/local/sendandsaveuid', (req: any, res: any) => {
+    uid = req.body;
+    writeUIDtoDB()
     res.status(200).send()
 })
  
-
-async function writeUIDtoDB() {
-        
-    // connect to mongodb 
-        const client = new MongoClient(uri)
-        const struid = uid.toString()
-        await client.connect
-        const db = client.db("main")
-
-        const visiscollection = db.collection(struid)
-        const usersCollection = client.       
-        // const result = await usersCollection.insertOne({ name: "Max", age: 30 });
-}
--- src/utils/getEvents.ts
+++ src/utils/getEvents.ts
@@ -6,12 +6,7 @@ export async function getEvents(req: app.Request)  {
 
     async function buttonEventAPI() {
         const requestBody = await req.body;
-<<<<<<< HEAD
 
-
-=======
-        
->>>>>>> 8c76e53ef400937cea6d475f6d3608ec6e93b037
         return requestBody
     }
 
